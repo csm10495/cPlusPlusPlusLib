3482e241c1f3f40692e8bc6e07e75cd87e64aabd
csm10495 <csm10495@gmail.com>
2015-08-23 19:54:52 -0700

    Add some starter code, version number.

--- cPlusPlusPlusLib/cPPPLib.cpp
+++ cPlusPlusPlusLib/cPPPLib.cpp
@@ -3,3 +3,16 @@
 * cPPPLib - A library of functions that should be in the C++ Standard Library
 * (C) - Charles Machalow - MIT License
 */
+
+#ifndef cPPPLib_CPP
+#define cPPPLib_CPP
+
+#include "cPPPLib.h"
+
+int main()
+{
+	std::cout << "cPlusPlusPlusLib Version " << cPPPLib_H::cPPPLib_V << std::endl << "(C) - Charles Machalow - MIT License" << std::endl;
+	return 1;
+}
+
+#endif cPPPLib_CPP
--- cPlusPlusPlusLib/cPPPLib.h
+++ cPlusPlusPlusLib/cPPPLib.h
@@ -3,3 +3,13 @@
  * cPPPLib - A library of functions that should be in the C++ Standard Library
  * (C) - Charles Machalow - MIT License
  */
+
+#ifndef cPPPLib_H
+#define cPPPLib_H
+
+#include <string>
+#include <iostream>
+
+static const std::string cPPPLib_V = ".1Alpha";
+
+#endif cPPPLib_H
