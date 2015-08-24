9489d764edec99aff39c461f6d8b73d8d618d351
csm10495 <csm10495@gmail.com>
2015-08-23 20:46:30 -0700

    Add StringFunctions class and two functions: splitIntoVector and splitIntoVectorByWhitespace. The functions are reminiscent of the Python .split function.

--- cPlusPlusPlusLib/cPPPLib.cpp
+++ cPlusPlusPlusLib/cPPPLib.cpp
@@ -9,9 +9,79 @@
 
 #include "cPPPLib.h"
 
+/// <summary>
+/// Splits the original_str into a std::vector by delimiter
+/// </summary>
+/// <param name="original_str">The original_str.</param>
+/// <param name="delim">The delimiter.</param>
+/// <returns></returns>
+std::vector<std::string> StringFunctions::splitIntoVector(const std::string &original_str, const std::string &delim)
+{
+	std::string working_str = original_str;
+	std::vector<std::string> ret_vec;
+
+	while (!working_str.empty())
+	{
+		unsigned int loc = working_str.find(delim);
+		// found in string
+		if (loc != std::string::npos)
+		{
+			ret_vec.push_back(working_str.substr(0, loc));
+			working_str = working_str.substr(loc + 1);
+		}
+		else
+		{
+			ret_vec.push_back(working_str);
+			break;
+		}
+	}
+
+	return ret_vec;
+}
+
+/// <summary>
+/// Splits the original_str into a std::vector by whitespace.
+/// </summary>
+/// <param name="original_str">The original_str.</param>
+/// <returns></returns>
+std::vector<std::string> StringFunctions::splitIntoVectorByWhitespace(const std::string &original_str)
+{
+	std::string working_str = original_str;
+	std::vector<std::string> ret_vec;
+
+	while (!working_str.empty())
+	{
+		unsigned int loc = working_str.find(" ");
+		// found in string
+		if (loc != std::string::npos)
+		{
+			if (working_str.substr(0, loc) != "")
+			{
+				ret_vec.push_back(working_str.substr(0, loc));
+			}
+			working_str = working_str.substr(loc + 1);
+		}
+		else
+		{
+			if (working_str != "")
+			{
+				ret_vec.push_back(working_str);
+			}
+			break;
+		}
+	}
+
+	return ret_vec;
+}
+
 int main()
 {
 	std::cout << "cPlusPlusPlusLib Version " << cPPPLib_H::cPPPLib_V << std::endl << "(C) - Charles Machalow - MIT License" << std::endl;
+	
+	std::string t = "a";
+	std::vector<std::string> tmp = StringFunctions::splitIntoVectorByWhitespace("     ");
+
+	system("PAUSE");
 	return 1;
 }
 
--- cPlusPlusPlusLib/cPPPLib.h
+++ cPlusPlusPlusLib/cPPPLib.h
@@ -8,8 +8,19 @@
 #define cPPPLib_H
 
 #include <string>
+#include <vector>
 #include <iostream>
 
 static const std::string cPPPLib_V = ".1Alpha";
+
+/// <summary>
+/// Class for functions relating to std::strings
+/// </summary>
+class StringFunctions
+{
+public:
+	static std::vector<std::string> splitIntoVector(const std::string &original_str, const std::string &delim);
+	static std::vector<std::string> splitIntoVectorByWhitespace(const std::string &original_str);
+};
 
 #endif cPPPLib_H
