8bc577eb80e66d913cf657a3ea0f375a9223a57f
csm10495 <csm10495@gmail.com>
2015-08-23 19:36:40 -0700

    Add starter cpp, h files

---
+++ cPlusPlusPlusLib/cPPPLib.h
@@ -0,0 +1,5 @@
+/*
+ * This is the main header file for cPlusPlusPlusLib
+ * cPPPLib - A library of functions that should be in the C++ Standard Library
+ * (C) - Charles Machalow - MIT License
+ */
